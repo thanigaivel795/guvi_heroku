220fe82d18f2a645db929efa899b0b5efb57e80e
thanigaivel795 <43550073+thanigaivel795@users.noreply.github.com>
2020-08-01 23:32:58 +0530

    CORS

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var request = new XMLHttpRequest()
 //request.open('GET',"https://dog.ceo/api/breeds/list/all",true);
-request.open('GET',"https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&mode=xml&appid=439d4b804bc8187953eb36d2a8c26a02",true);
+
+//request.open('GET',"https://cors-anywhere.herokuapp.com/https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com",true);
+request.open('GET',"https://cors-anywhere.herokuapp.com/https://samples.openweathermap.org/data/2.5/forecast/hourly?q=London,us&mode=xml&appid=439d4b804bc8187953eb36d2a8c26a02",true);
+
 request.onload = function(){
     var data = JSON.parse(this.response)
     console.log(data,"response")
+
+    // for weather forecast URL the response is not JSON it is HTML
+   //console.log(this.response)
 }
 request.send()
+
+
+
+
+
 // fetch('https://api.domainsdb.info/v1/domains/search?domain=facebook&zone=com',{
 //     mode: 'cors'
 //   })
